63ed6195ef946da9b9eb35bd103363b51e7a9bd0
Sergey Shikanov <sshikanov08@gmail.com>
2025-06-25 09:11:38 +0300

    added TextBoxTest

---
+++ project/src/test/java/TextBoxTest.java
@@ -0,0 +1,32 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Configuration.*;
+
+public class TextBoxTest {
+
+    //https://demoqa.com/text-box
+
+    @BeforeAll
+    public static void setup() {
+        pageLoadTimeout = 60_000; // 60 секунд вместо стандартных 30
+        timeout = 15_000; // Увеличьте общий таймаут
+    }
+    @Test
+    void fillFormTest(){
+        Configuration.holdBrowserOpen = true;
+
+         open("https://demoqa.com/text-box");
+         $("[id=userName]").setValue("Shikanov");
+        $("[id=userEmail]").setValue("sshik@gmail.com");
+        $("[id=currentAddress]").setValue("Moscow");
+        $("[id=permanentAddress]").setValue("Novosib");
+        $("[class='btn btn-primary']").click();
+        $("[id=output]").shouldHave(text("Shikanov"),text("sshik@gmail.com"),text("Moscow"),text("Novosib"));
+
+    }
+}
